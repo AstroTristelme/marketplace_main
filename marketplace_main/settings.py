1d9e9ea8cae9d132f444d0e48b59d587c8a3ce94
AstroTristelme <sxriustumbado@gmail.com>
2025-11-10 11:15:52 -0800

    Agregue forms.py para el sistema de registro de usuarios

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -124,3 +124,22 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
