89e74cf1b26ca03e6c8acd8ebd7d11c37f491b82
AstroTristelme <sxriustumbado@gmail.com>
2025-11-11 12:19:46 -0800

    Agruegue funcionalidadpara Login y Registro de usuario con su interface

--- store/forms.py
+++ store/forms.py
@@ -3,7 +3,6 @@ forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
 from .models import Item
 
 class LoginForm(AuthenticationForm):
